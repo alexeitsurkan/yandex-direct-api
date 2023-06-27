3262696d399635d865218e746a0516777b89bfef
alex <alexeitsurkan@gmail.com>
2023-06-27 13:26:06 +0300

    AttributionModel AUTO

--- src/services/campaigns/enum/AttributionModelEnum.php
+++ src/services/campaigns/enum/AttributionModelEnum.php
@@ -7,12 +7,13 @@ use directapi\components\Enum;
 
 class AttributionModelEnum extends Enum
 {
-    public const FC = "FC";
-    public const LC = "LC";
-    public const LSC = "LSC";
-    public const LYDC = "LYDC";
-    public const FCCD = "FCCD";
-    public const LCCD = "LCCD";
-    public const LSCCD = "LSCCD";
+    public const AUTO   = "AUTO";
+    public const FC     = "FC";
+    public const LC     = "LC";
+    public const LSC    = "LSC";
+    public const LYDC   = "LYDC";
+    public const FCCD   = "FCCD";
+    public const LCCD   = "LCCD";
+    public const LSCCD  = "LSCCD";
     public const LYDCCD = "LYDCCD";
 }
